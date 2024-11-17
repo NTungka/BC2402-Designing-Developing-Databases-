7a0cb193b8be87ed4ec1294469b1cedea0221e6f
Divyansh Jain <114384181+Divyansh0987@users.noreply.github.com>
2024-11-17 14:48:28 +0800

    Updated Q2

--- NoSQLQueries.js
+++ NoSQLQueries.js
@@ -23,14 +23,16 @@ db.customerSupport.aggregate(
         { 
             $match: { 
                 category: {  $in: ["ACCOUNT", "CANCEL", "CONTACT", "DELIVERY", "FEEDBACK", "INVOICE", "ORDER", "PAYMENT", "REFUND", "SHIPPING", "SUBSCRIPTION"] },
-                flags: {$regex: "[Q|W]"}
-            } 
-        },
+                flags: {$regex: /[QW]/}
+            }
+        }
         {
         $group: {
             _id: "$category",
             Colloquial: { $sum: { $cond: [{ $regexMatch: { input: "$flags", regex: /Q/ } }, 1, 0] } },
-            Offensive: { $sum: { $cond: [{ $regexMatch: { input: "$flags", regex: /W/ } }, 1, 0] } } 
+            Offensive: { $sum: { $cond: [{ $regexMatch: { input: "$flags", regex: /W/ } }, 1, 0] } },
+            Both: { $sum: { $cond: [{ $regexMatch: { input: "$flags", regex: /(?=.*Q)(?=.*W)/ } }, 1, 0] } },
+            Other: { $sum: { $cond: [{ $regexMatch: { input: "$flags", regex: /(?=.*Q)(?=.*W)/ } }, 0, 1] } },
             }
         }
 ])
