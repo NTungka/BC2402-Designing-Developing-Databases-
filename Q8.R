2ea300faddf4a5750e3d68d057bea51e59c7bae1
Divyansh Jain <114384181+Divyansh0987@users.noreply.github.com>
2024-11-19 20:26:20 +0800

    Added code for text mining in R

---
+++ Q8.R
@@ -0,0 +1,48 @@
+pacman::p_load(tidyverse, data.table, tidytext, textstem, stringr)
+
+dt.reviews <- fread("Q8.csv")
+
+# dt.reviews %>%
+#   filter(Airline == "Qatar Airways") %>%
+#   unnest_tokens(word, Reviews) %>%
+#   anti_join(stop_words) %>%
+#   count(Airline, word, sort = TRUE) %>%
+#   bind_tf_idf(Airline, word, n)
+
+bigram <- c("word1", "word2")
+
+dt.reviews %>%
+  filter(Airline == "Qatar Airways") %>%
+  unnest_tokens(word, Reviews, token = "ngrams", n = 2) %>%
+  filter(!grepl('[0-9]', word)) %>%
+  anti_join(stop_words) %>%
+  separate(word, bigram, sep = " ") %>%
+  mutate(
+    word1 = lemmatize_words(word1),
+    word2 = lemmatize_words(word2)
+  ) %>%
+  filter(!word1 %in% stop_words$word,
+         !word2 %in% stop_words$word) %>%
+  unite(word, word1, word2, sep = " ") %>%
+  count(Airline, word, sort = TRUE) %>%
+  bind_tf_idf(word, Airline, n) %>%
+  arrange(desc(tf_idf)) %>%
+  head(n = 20)
+
+dt.reviews %>%
+  filter(Airline == "Singapore Airlines") %>%
+  unnest_tokens(word, Reviews, token = "ngrams", n = 2) %>%
+  filter(!grepl('[0-9]', word)) %>%
+  anti_join(stop_words) %>%
+  separate(word, bigram, sep = " ") %>%
+  mutate(
+    word1 = lemmatize_words(word1),
+    word2 = lemmatize_words(word2)
+  ) %>%
+  filter(!word1 %in% stop_words$word,
+         !word2 %in% stop_words$word) %>%
+  unite(word, word1, word2, sep = " ") %>%
+  count(Airline, word, sort = TRUE) %>%
+  bind_tf_idf(word, Airline, n) %>%
+  arrange(desc(tf_idf)) %>%
+  head(n = 20)
