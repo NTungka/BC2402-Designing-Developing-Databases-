2faabd69bc7f83b8da518e74f22d5e83486dae30
Divyansh Jain <114384181+Divyansh0987@users.noreply.github.com>
2024-11-19 20:43:16 +0800

    Create Q8.sql

---
+++ Q8.sql
@@ -0,0 +1,15 @@
+use bc2402_gp;
+
+show tables;
+
+SET SESSION group_concat_max_len = 100000000000;
+
+SELECT Airline, GROUP_CONCAT(Reviews SEPARATOR ' ') as Reviews
+FROM airlines_reviews
+WHERE Verified = 'TRUE' and Recommended = 'no'
+GROUP BY Airline;
+
+SELECT Airline, COUNT(Reviews) as NumOfReviews
+FROM airlines_reviews
+WHERE Verified = 'TRUE' and Recommended = 'no'
+GROUP BY Airline;
