107dc38494beb12c18aaa2ba0778ed13da388fd0
Divyansh Jain <114384181+Divyansh0987@users.noreply.github.com>
2024-11-20 21:51:18 +0800

    Update Q6.R

--- Q6.R
+++ Q6.R
@@ -16,17 +16,22 @@ dt.q6 %>%
   ggplot(aes(x=avg_stay_flight_ratio, y=avg_baggage_flight_ratio)) +
   geom_point() +
   geom_abline(slope = 0.007019, intercept = 0.073887) +
+  annotate(geom="text", label=expression("R"^2*" = 0.1118"), x = 18, y = 0.125, size = 5) +
   theme_minimal(base_family = "serif") +
-  labs(x="Average")
+  labs(x="Mean Wants of Extra Baggage to Mean Flight Duration Ratio ", y = "Mean Length of Stay to Mean Flight Duration Ratio")
 
 dt.q6 %>%
   ggplot(aes(x=avg_stay_flight_ratio, y=avg_seat_flight_ratio)) +
   geom_point() +
   geom_abline(slope = -0.0005775, intercept = 0.0445806) +
-  theme_minimal(base_family = "serif")
+  annotate(geom="text", label=expression("R"^2*" = 0.0004"), x = 18, y = 0.15, size = 5) +
+  theme_minimal(base_family = "serif") +
+  labs(x="Mean Wants of Preferred Seat to Mean Flight Duration Ratio ", y = "Mean Length of Stay to Mean Flight Duration Ratio")
 
 dt.q6 %>%
   ggplot(aes(x=avg_stay_flight_ratio, y=avg_meals_flight_ratio)) +
   geom_point() +
   geom_abline(slope = 0.003284, intercept = 0.049313) +
-  theme_minimal(base_family = "serif")
+  annotate(geom="text", label=expression("R"^2*" = 0.0309"), x = 18, y = 0.15, size = 5) +
+  theme_minimal(base_family = "serif") +
+  labs(x="Mean Wants of In-Flight Meals to Mean Flight Duration Ratio ", y = "Mean Length of Stay to Mean Flight Duration Ratio")
