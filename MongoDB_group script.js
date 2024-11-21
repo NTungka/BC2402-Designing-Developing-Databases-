7011761571e9b91a8d8cec6ab7ad3b6c8aa43401
NTungka <120771436+NTungka@users.noreply.github.com>
2024-11-21 20:43:21 +0800

    Add files via upload

--- MongoDB_group script.js
+++ MongoDB_group script.js
@@ -20,13 +20,11 @@ db.customer_support.aggregate([
     },
     {
         $group:{
-            _id:"$category"
+            _id:"$category",
+            count: {$count: {}
         }
-    }, 
-    {
-        $count: "NumberofCategories"
-    }
-    ])
+    }])
+
 
 //Q2    
 
@@ -39,11 +37,9 @@ db.customer_support.aggregate([
             _id: "$category",
             Colloquial: { $sum: { $cond: [{ $regexMatch: { input: "$flags", regex: /Q/ } }, 1, 0] } },
             Offensive: { $sum: { $cond: [{ $regexMatch: { input: "$flags", regex: /W/ } }, 1, 0] } },
+            Both: { $sum: { $cond: [{ $regexMatch: { input: "$flags", regex: /(?=.*Q)(?=.*W)/ } }, 1, 0] } },
             Others: { $sum: { $cond: [{ $and: [ { $not: [{ $regexMatch: { input: "$flags", regex: /Q/ } }] }, { $not: [{ $regexMatch: { input: "$flags", regex: /W/ } }] } ] }, 1, 0] } }
         }
-    },
-    {
-        $project: { _id: 0, category: "$_id", Colloquial: 1, Offensive: 1, Others: 1 }
     }
 ]);
 
@@ -142,6 +138,7 @@ db.customer_booking.aggregate([
   }
 ]);
 
+
 //Q7 
 db.airlines_reviews.aggregate([
     {
@@ -178,9 +175,280 @@ db.airlines_reviews.aggregate([
         $sort: { Airline: 1, Class: 1, Period: 1 }}
 ]);
 
-//Q8
+use "airline"
 
-//Q9
+// Q8
+db.airlines_reviews.aggregate([
+    {
+        $match: {
+            Verified: "TRUE",
+            Recommended: "no"
+        }
+    },
+    {
+        $group: { 
+            _id: {Airline: "$Airline"},
+            AvgSeatComfort: {$avg: "$SeatComfort"},
+            AvgStaffService: {$avg: "$StaffService"},
+            AvgFoodnBeverages: {$avg: "$FoodnBeverages"},
+            AvgInflightEntertainment: {$avg: "$InflightEntertainment"},
+            AvgValueForMoney: {$avg: "$ValueForMoney"},
+        }
+    }
+])
+
+db.airlines_reviews.aggregate([
+    {
+        $match: {
+            Verified: "TRUE",
+            Recommended: "no"
+        }
+    },
+    {
+        $group: { 
+            _id: {Airline: "$Airline", TypeOfTraveller: "$TypeofTraveller"},
+            AvgSeatComfort: {$avg: "$SeatComfort"},
+            AvgStaffService: {$avg: "$StaffService"},
+            AvgFoodnBeverages: {$avg: "$FoodnBeverages"},
+            AvgInflightEntertainment: {$avg: "$InflightEntertainment"},
+            AvgValueForMoney: {$avg: "$ValueForMoney"},
+        }
+    }
+])
+
+
+db.airlines_reviews.aggregate([
+  {
+    $match: {
+      Verified: "TRUE",
+      Recommended: "no"
+    }
+  },
+  {
+    $group: {
+      _id: "$Airline",
+      allReviews: {
+        $push: "$Reviews"
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      Airline: "$_id",
+      ConcatenatedReviews: {
+        $reduce: {
+          input: "$allReviews",
+          initialValue: "",
+          in: { $concat: ["$$value", " ", "$$this"] }
+        }
+      }
+    }
+  }
+])
+
+db.airlines_reviews.aggregate([
+  {
+    $match: {
+      Verified: "TRUE",
+      Recommended: "no"
+    }
+  },
+  {
+    $group: {
+      _id: "$TypeofTraveller",
+      allReviews: {
+        $push: "$Reviews"
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      TypeofTraveller: "$_id",
+      ConcatenatedReviews: {
+        $reduce: {
+          input: "$allReviews",
+          initialValue: "",
+          in: { $concat: ["$$value", " ", "$$this"] }
+        }
+      }
+    }
+  }
+])
+
+// Q9
+
+// Pre-Covid
+db.airlines_reviews.aggregate([
+    {
+        $match: {Airline: 'Singapore Airlines'}
+    },
+    {
+    $project: {
+        date: {
+            $split: ["$MonthFlown", "-"],
+            },
+        review: "$Reviews"
+        }
+    },
+    {
+    $addFields: {
+        month: {$switch: {
+              branches: [
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Jan"] }, then: 1 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Feb"] }, then: 2 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Mar"] }, then: 3 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Apr"] }, then: 4 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "May"] }, then: 5 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Jun"] }, then: 6 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Jul"] }, then: 7 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Aug"] }, then: 8 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Sep"] }, then: 9 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Oct"] }, then: 10 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Nov"] }, then: 11 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Dec"] }, then: 12 }
+              ],
+              default: null
+            }},
+            year: { $toInt: { $arrayElemAt: ["$date", 1] } }
+        }
+    },
+    {
+        $match: { month : { $lte: 3 }, year : { $lt : 20 } }
+    },
+    {
+        $group: {
+            _id: null,
+            allReviews: {
+                $push: "$review"
+              }
+        }
+    },
+    {
+        $project: {
+            concatenatedReviews: { $reduce: { 
+                input: "$allReviews", 
+                initialValue: "", 
+                in: { $concat: ["$$value", " ", "$$this"] } 
+            }}
+        }
+    }
+])
+
+// During COVID
+db.airlines_reviews.aggregate([
+    {
+        $match: {Airline: 'Singapore Airlines'}
+    },
+    {
+    $project: {
+        date: {
+            $split: ["$MonthFlown", "-"],
+            },
+        review: "$Reviews"
+        }
+    },
+    {
+    $addFields: {
+        month: {$switch: {
+              branches: [
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Jan"] }, then: 1 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Feb"] }, then: 2 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Mar"] }, then: 3 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Apr"] }, then: 4 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "May"] }, then: 5 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Jun"] }, then: 6 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Jul"] }, then: 7 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Aug"] }, then: 8 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Sep"] }, then: 9 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Oct"] }, then: 10 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Nov"] }, then: 11 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Dec"] }, then: 12 }
+              ],
+              default: null
+            }},
+            year: { $toInt: { $arrayElemAt: ["$date", 1] } }
+        }
+    },
+    {
+        $match: { month : { $lt: 5 , $gte: 3}, year : { $gte : 20 , $lt: 23} }
+    },
+    {
+        $group: {
+            _id: null,
+            allReviews: {
+                $push: "$review"
+              }
+        }
+    },
+    {
+        $project: {
+            concatenatedReviews: { $reduce: { 
+                input: "$allReviews", 
+                initialValue: "", 
+                in: { $concat: ["$$value", " ", "$$this"] } 
+            }}
+        }
+    }
+])
+
+// Post COVID
+db.airlines_reviews.aggregate([
+    {
+        $match: {Airline: 'Singapore Airlines'}
+    },
+    {
+    $project: {
+        date: {
+            $split: ["$MonthFlown", "-"],
+            },
+        review: "$Reviews"
+        }
+    },
+    {
+    $addFields: {
+        month: {$switch: {
+              branches: [
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Jan"] }, then: 1 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Feb"] }, then: 2 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Mar"] }, then: 3 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Apr"] }, then: 4 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "May"] }, then: 5 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Jun"] }, then: 6 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Jul"] }, then: 7 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Aug"] }, then: 8 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Sep"] }, then: 9 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Oct"] }, then: 10 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Nov"] }, then: 11 },
+                { case: { $eq: [{$arrayElemAt: ["$date", 0]}, "Dec"] }, then: 12 }
+              ],
+              default: null
+            }},
+            year: { $toInt: { $arrayElemAt: ["$date", 1] } }
+        }
+    },
+    {
+        $match: { month : { $gte: 5 }, year : { $gte : 23 } }
+    },
+    {
+        $group: {
+            _id: null,
+            allReviews: {
+                $push: "$review"
+              }
+        }
+    },
+    {
+        $project: {
+            concatenatedReviews: { $reduce: { 
+                input: "$allReviews", 
+                initialValue: "", 
+                in: { $concat: ["$$value", " ", "$$this"] } 
+            }}
+        }
+    }
+])
 
 
 //Q10 Can be changed to customer_support, switching "Reviews" to "instruction" and removing Name
